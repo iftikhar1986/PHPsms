6b027befa6d6c03e4ab9423321832038e23e675e
iftikhar1986 <iftikhar.uddin.fazal@gmail.com>
2021-01-03 01:12:03 +0300

    created database connection

---
+++ dbcon.php
@@ -0,0 +1,14 @@
+<?php
+
+    $con = mysqli_connect('localhost','root','','phpsms');
+
+    if ($con == false) {
+        echo "Connection is not done";
+    } 
+    
+    // else {
+    //     echo "Connected DB";
+    // }
+    
+
+?>
---
+++ readme.txt
@@ -0,0 +1,13 @@
+admin
+	Id
+	username
+	password
+
+Student
+	id
+	rollno
+	name
+	city
+	parants contact number
+	standerd
+	image
